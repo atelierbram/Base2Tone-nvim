77c31bcc86c79705e394f21ff34fc52758467cba
atelierbram <bramdeh@gmail.com>
2022-10-22 21:07:37 +0200

    add missing files for lualine

--- lua/lualine/themes/base2tone_desert_light.lua
+++ lua/lualine/themes/base2tone_desert_light.lua
@@ -3,10 +3,42 @@
 -- Credit: Zoltan Dalmadi(lightline)
 -- LuaFormatter off
 local colors = {
+B2T_A0 =  "#292724",
+B2T_A1 =  "#3d3a34",
+B2T_A2 =  "#615c51",
+B2T_A3 =  "#7e7767",
+B2T_A4 =  "#908774",
+B2T_A5 =  "#9f9684",
+B2T_A6 =  "#ada594",
+B2T_A7 =  "#bbb4a5",
 
-Warning: include_once(../../schemes/lualine/vars-hex-b2t_desert.lua.php): failed to open stream: No such file or directory in /Users/bram/Sites/bitbucket/base2tone-nvim/db/views/lualine/b2t_desert_light.lua.php on line 11
+B2T_B0 =  "#5c523d",
+B2T_B1 =  "#6e6045",
+B2T_B2 =  "#816f4b",
+B2T_B3 =  "#957e50",
+B2T_B4 =  "#ac8e53",
+B2T_B5 =  "#c6ad7b",
+B2T_B6 =  "#ddcba6",
+B2T_B7 =  "#f2ead9",
 
-Warning: include_once(): Failed opening '../../schemes/lualine/vars-hex-b2t_desert.lua.php' for inclusion (include_path='.:pear/share/pear:/usr/local/Cellar/php@7.4/7.4.30/share/pear') in /Users/bram/Sites/bitbucket/base2tone-nvim/db/views/lualine/b2t_desert_light.lua.php on line 11
+B2T_C0 =  "#847b75",
+B2T_C1 =  "#978d87",
+B2T_C2 =  "#a89f99",
+B2T_C3 =  "#b9b1ac",
+B2T_C4 =  "#cac3be",
+B2T_C5 =  "#dcd5d0",
+B2T_C6 =  "#ede7e3",
+B2T_C7 =  "#fbfaf9",
+
+B2T_D0 =  "#bc672f",
+B2T_D1 =  "#d37231",
+B2T_D2 =  "#dd7c3c",
+B2T_D3 =  "#e58748",
+B2T_D4 =  "#ec9255",
+B2T_D5 =  "#f29d63",
+B2T_D6 =  "#f8a872",
+B2T_D7 =  "#ffb380",
+
 }
 
 -- LuaFormatter on
--- lua/lualine/themes/base2tone_morning_dark.lua
+++ lua/lualine/themes/base2tone_morning_dark.lua
@@ -3,10 +3,42 @@
 -- Credit: Zoltan Dalmadi(lightline)
 -- LuaFormatter off
 local colors = {
+B2T_A0 =  "#232834",
+B2T_A1 =  "#31363f",
+B2T_A2 =  "#4f5664",
+B2T_A3 =  "#656e81",
+B2T_A4 =  "#707a8f",
+B2T_A5 =  "#7e889a",
+B2T_A6 =  "#8d95a5",
+B2T_A7 =  "#a9afbc",
 
-Warning: include_once(/Users/bram/Sites/bitbucket/base2tone-nvim/db/schemes/lualine/vars-hex-b2t_morning.lua.php): failed to open stream: No such file or directory in /Users/bram/Sites/bitbucket/base2tone-nvim/db/views/lualine/b2t_morning_dark.lua.php on line 11
+B2T_B0 =  "#063289",
+B2T_B1 =  "#0b3c9d",
+B2T_B2 =  "#1659df",
+B2T_B3 =  "#3d75e6",
+B2T_B4 =  "#728fcb",
+B2T_B5 =  "#93abdc",
+B2T_B6 =  "#b7c9eb",
+B2T_B7 =  "#dee6f7",
 
-Warning: include_once(): Failed opening '/Users/bram/Sites/bitbucket/base2tone-nvim/db/schemes/lualine/vars-hex-b2t_morning.lua.php' for inclusion (include_path='.:pear/share/pear:/usr/local/Cellar/php@7.4/7.4.30/share/pear') in /Users/bram/Sites/bitbucket/base2tone-nvim/db/views/lualine/b2t_morning_dark.lua.php on line 11
+B2T_C0 =  "#544d40",
+B2T_C1 =  "#69604f",
+B2T_C2 =  "#867b65",
+B2T_C3 =  "#9c927c",
+B2T_C4 =  "#b6ad9a",
+B2T_C5 =  "#cdc4b1",
+B2T_C6 =  "#e3dcce",
+B2T_C7 =  "#faf8f5",
+
+B2T_D0 =  "#2d2006",
+B2T_D1 =  "#594212",
+B2T_D2 =  "#896724",
+B2T_D3 =  "#9a7c42",
+B2T_D4 =  "#b29762",
+B2T_D5 =  "#c6b28b",
+B2T_D6 =  "#d1c2a3",
+B2T_D7 =  "#e5ddcd",
+
 }
 
 -- LuaFormatter on
